4d65db6c7519a52040b14da00055cb5f4d458d94
Raju Mannem <rajumannem71@gmail.com>
2025-02-23 17:25:16 +0530

    dashboard page v1

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Colleges from "@/components/Colleges";
+
+const Dashboard = () => {
+  return (
+    <main className="h-full w-full">
+        <Colleges/>
+    </main>
+  )
+}
+
+export default Dashboard
