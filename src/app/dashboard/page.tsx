663e79e71605de5a0e5f306a5b969576cdfa4429
Raju Mannem <rajumannem71@gmail.com>
2025-03-02 21:58:05 +0530

    dashboard usestate added for colleges and courses

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,32 @@
+"use client"
+import { useState } from "react";
 import Colleges from "@/components/Colleges";
+import College from "@/components/College";
 
 const Dashboard = () => {
+  const [selectType, setSelectType] = useState<string>("");
+  const handleSelect = (option: string) => {
+    setSelectType(option);
+  }
   return (
     <main className="h-full w-full">
-        <Colleges/>
+      <section className="flex justify-end items-center px-12 py-4 my-4 text-[8px] sm:text-sm font-sans bg-indigo-200">
+        <article className="px-2 mx-8 bg-stone-50 rounded-sm">
+        <button onClick={()=>handleSelect("colleges")} className="focus:outline-0">
+          Colleges
+        </button>
+        </article>
+        <article className="px-2 bg-stone-50 rounded-sm">
+        <button onClick={()=>handleSelect("courses")} className="focus:outline-0">
+          Courses
+        </button>
+        </article>
+      </section>
+      {(selectType=="courses")
+      ?(<Colleges/>)
+      :(<College/>)      
+    }
+        
     </main>
   )
 }
