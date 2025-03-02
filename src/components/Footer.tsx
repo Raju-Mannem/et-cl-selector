b6a494225761c5a6861741bafda95902491b3010
Raju Mannem <rajumannem71@gmail.com>
2025-03-02 21:54:37 +0530

    footer v2 update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="px-16 py-4 flex justify-center items-center bg-slate-50
-    border shadow-xl shadow-blue-500/10 text-blue-300 text-sm">
+    <footer className="py-4 flex justify-center items-center bg-slate-50
+    border shadow-xl shadow-blue-500/10 text-blue-300 text-[10px] sm:text-sm">
         <strong>&copy; Copy Rights Reserved By Campus Logics {new Date().getFullYear()}</strong>
     </footer>
   )
