4644d731b0aae498e5f7b322ba5c6145da0ed443
Raju Mannem <rajumannem71@gmail.com>
2025-03-13 12:47:36 +0530

    added pading to dasboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
     setSelectType(option);
   }
   return (
-    <main className="h-full w-full">
+    <main className="h-full w-full px-2">
       <section className="flex justify-end items-center px-12 py-4 my-4 text-[8px] sm:text-sm font-sans bg-indigo-200">
         <article className="px-2 mx-8 bg-stone-50 rounded-sm">
         <button onClick={()=>handleSelect("colleges")} className="focus:outline-0">
