567eef4b498cf2c283a16eb9ee335779a8d3f382
Raju Mannem <rajumannem71@gmail.com>
2025-05-18 02:46:37 +0530

    cutoff component added and updated to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,51 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Colleges from "@/components/Colleges";
 import College from "@/components/College";
+import Cutoff2024 from "@/components/Cutoff2024";
 
 const Dashboard = () => {
   const [selectType, setSelectType] = useState<string>("");
   const handleSelect = (option: string) => {
     setSelectType(option);
-  }
+  };
   return (
     <main className="h-full w-full px-2">
       <section className="flex justify-end items-center px-12 py-4 my-4 text-[8px] sm:text-sm font-sans bg-indigo-200">
         <article className="px-2 mx-8 bg-stone-50 rounded-sm">
-        <button onClick={()=>handleSelect("colleges")} className="focus:outline-0">
-          Colleges
-        </button>
+          <button
+            onClick={() => handleSelect("colleges")}
+            className="focus:outline-0"
+          >
+            Colleges
+          </button>
         </article>
         <article className="px-2 bg-stone-50 rounded-sm">
-        <button onClick={()=>handleSelect("courses")} className="focus:outline-0">
-          Courses
-        </button>
+          <button
+            onClick={() => handleSelect("courses")}
+            className="focus:outline-0"
+          >
+            Courses
+          </button>
+        </article>
+        <article className="px-2 bg-stone-50 rounded-sm">
+          <button
+            onClick={() => handleSelect("cutoff2024")}
+            className="focus:outline-0"
+          >
+            Cutoff 2024
+          </button>
         </article>
       </section>
-      {(selectType=="courses")
-      ?(<Colleges/>)
-      :(<College/>)      
-    }
-        
+      {selectType === "courses" ? (
+        <Colleges />
+      ) : selectType === "colleges" ? (
+        <College />
+      ) : (
+        <Cutoff2024 />
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
