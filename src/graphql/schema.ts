98295fb48e85e877a7b1ce9f34ab9aa1a382539d
Raju Mannem <rajumannem71@gmail.com>
2025-07-01 13:41:55 +0530

    2024 cutff added to database & schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -58,6 +58,41 @@ type College {
     priority: Int
   }
 
+  type ApCutoff2024 {
+    sno: ID!
+    inst_code: String
+    institute_name: String
+    place: String
+    dist_code: String
+    co_education: String
+    college_type: String
+    year_of_estab: Float
+    branch_code: String
+    branch_name: String
+    oc_boys: Int
+    oc_girls: Int
+    bc_a_boys: Int
+    bc_a_girls: Int
+    bc_b_boys: Int
+    bc_b_girls: Int
+    bc_c_boys: Int
+    bc_c_girls: Int
+    bc_d_boys: Int
+    bc_d_girls: Int
+    bc_e_boys: Int
+    bc_e_girls: Int
+    sc_boys: Int
+    sc_girls: Int
+    st_boys: Int
+    st_girls: Int
+    ews_girls_ou: Int
+    tuition_fee: Int
+    affiliated_to: String
+    inst_reg: String
+    a_reg: String
+    priority: Int
+  }
+
 input ApCutoff2023Input {
 sno: ID!
 inst_code: String
@@ -123,6 +158,18 @@ priority: Int
     dynamicCastes: JSON
   }
 
+  type ApCutoff2024Dynamic {
+    sno: ID!
+    inst_code: String
+    institute_name: String
+    place: String
+    dist_code: String
+    branch_name: String
+    branch_code: String
+    co_education: String
+    dynamicCastes: JSON
+  }
+
   scalar JSON
 
 
@@ -134,6 +181,7 @@ priority: Int
     apCutoff2023(sno: Float!): ApCutoff2023
     apCutoff2023sByInstCodes(inst_codes: [String!]!): [ApCutoff2023!]!
     apCutoff2023sByRank(filter: RankFilterInput!): [ApCutoff2023Dynamic!]!
+    apCutoff2024sByRank(filter: RankFilterInput!): [ApCutoff2023Dynamic!]!
     apCutoff2023sByInstDist(filter: InstDistFilterInput!): [ApCutoff2023Dynamic!]!
   }
 
